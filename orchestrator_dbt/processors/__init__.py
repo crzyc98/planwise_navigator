fca9df99668d2fb1920b3367b2c718d8357a365b
crzyc <crzyc98@gmail.com>
2025-08-12 11:00:57 -0400

    removing legacy dbt orchestrator

--- orchestrator_dbt/processors/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Data processing modules for workforce and census operations."""
-
-from .census_processor import CensusProcessor
-from .workforce_processor import WorkforceProcessor
-
-__all__ = ["CensusProcessor", "WorkforceProcessor"]
--- orchestrator_dbt/validators/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Validation and quality check frameworks."""
-
-from .data_quality_validator import DataQualityValidator
-from .schema_validator import SchemaValidator
-from .business_logic_validator import BusinessLogicValidator
-
-__all__ = ["DataQualityValidator", "SchemaValidator", "BusinessLogicValidator"]
